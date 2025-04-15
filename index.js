067934efb04482b9ca6cff7c339e1abdf08643e1
Aditya <helloadig@gmail.com>
2025-04-15 13:13:23 +0530

    jokes api

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get('/twitter', (req, res) => {
   res.send('dev_aditya')
 })
 
+app.get('/jokes', (req, res) => {
+  res.json({
+    joke: 'Why did the chicken cross the road? To get the other side!',
+    punchline: 'But why did the chicken cross the road? to get the other side!',
+  })
+})
+
 app.get('/login', (req, res) => {
   res.send('<h1>Hello World from the login...</h1>')
 })
